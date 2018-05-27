3ed825d0358c6459606295f8d5ffb2c5fdfe0c52
FrancoDegno <francodegano@hotmail.com>
2018-05-27 15:03:29 -0300

    tdfhg
    drysdrg

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php 
+	$server ='localhost';
+	$user= 'root';
+	$pass= '';
+	$database= 'test_juegos';
+
+	$conection = mysqli_connect($server,$user,$pass,$database);
+
+	if(!$conection)
+	{
+		echo 'Error Connection';
+	}
+
+
+	$uss = $_GET['ussA'];
+	$pss= $_GET['pssB'];
+
+	$sql = "SELECT * FROM usuarios WHERE usuario LIKE '$uss' AND password LIKE '$pss'";
+
+	$query=mysqli_query($conection,$sql);
+	 
+
+	if(mysqli_num_rows($query)>0)
+		echo "Consulta Excitosa";
+	else
+		echo "Consula Erronea";
+
+
+ ?>
---
+++ test2.php
@@ -0,0 +1,27 @@
+<?php 
+	$server ='localhost';
+	$user= 'root';
+	$pass= '';
+	$database= 'test_juegos';
+
+	$conection = mysqli_connect($server,$user,$pass,$database);
+
+	if(!$conection)
+	{
+		echo 'Error Connection';
+	}
+
+
+	$uss = $_GET['user'];
+	$pss = $_GET['pass'];
+	$sc  = $_GET['score'];
+
+	$sql = "SELECT * FROM usuarios WHERE usuario LIKE '$uss'";
+
+	$query=mysqli_query($conection,$sql);
+	 
+	if(mysqli_num_rows($conection)=0)
+		$sql="INSERT INTO 'usuarios'('id','usuario','password','score') VALUES (NULL,$uss,$pss,$sc)";
+
+
+ ?>
